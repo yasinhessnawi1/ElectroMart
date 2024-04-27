e90d1884f9971a77432a16d6e85bd7d96a67c0b4
yasinhessnawi1 <79910859+yasinhessnawi1@users.noreply.github.com>
2024-04-27 17:13:44 +0200

    Updates

---
+++ precache-manifest.7b7eb0e7ffcf5812e49f7c5c5692ab2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "168e045c7a8f15c737f798ddfb8087ec",
+    "url": "/ElectroMart/index.html"
+  },
+  {
+    "revision": "7abbe267d3533b7f16c6",
+    "url": "/ElectroMart/static/js/2.23efe962.chunk.js"
+  },
+  {
+    "revision": "23648192e40aba299792de6ab60b8298",
+    "url": "/ElectroMart/static/js/2.23efe962.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6d234a5cb62ade420d0",
+    "url": "/ElectroMart/static/js/main.9c6ae5cc.chunk.js"
+  },
+  {
+    "revision": "fb04d735386394c64c4c",
+    "url": "/ElectroMart/static/js/runtime-main.b4901f6a.js"
+  },
+  {
+    "revision": "f04ee572e46bdbf07a3acb6d96ece0e1",
+    "url": "/ElectroMart/static/media/logo.f04ee572.png"
+  }
+]);
