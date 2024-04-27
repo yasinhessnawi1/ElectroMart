ea46121767296dff3ff84b090618f0c74cf60472
yasinhessnawi1 <79910859+yasinhessnawi1@users.noreply.github.com>
2024-04-27 18:12:25 +0200

    added a redirection file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ProductPage from './pages/ProductPage';
 import CartPage from './pages/CartPage';
@@ -7,21 +7,24 @@ import { CartProvider } from './context/CartContext';
 import { UserProvider } from './context/UserContext';
 
 
+import { BrowserRouter as Router } from 'react-router-dom';
+
 function App() {
   return (
     <UserProvider>
-  <CartProvider>
-    <Router>
-      <Routes>
-        <Route path="/ElectroMart" element={<HomePage />} exact />
-        <Route path="/ElectroMart/product/:id" element={<ProductPage />} />
-        <Route path="/ElectroMart/cart" element={<CartPage />} />
-      </Routes>
-    </Router>
-  </CartProvider>
-</UserProvider>
-)
-  ;
+      <CartProvider>
+        <Router basename="/ElectroMart">
+          <Routes>
+            <Route path="/" element={<HomePage />} exact />
+            <Route path="/product/:id" element={<ProductPage />} />
+            <Route path="/cart" element={<CartPage />} />
+          </Routes>
+        </Router>
+      </CartProvider>
+    </UserProvider>
+  );
 }
 
 export default App;
+
+
